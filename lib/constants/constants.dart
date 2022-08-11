ccf552ba749c675fa1dc624d9a7e57f0682d6040
quocbao64 <quocbao642002@gmail.com>
2022-08-11 22:07:14 +0700

    add: order-detail page

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 83, 232, 139);
+const kPrimaryColor2 = Color.fromARGB(255, 21, 190, 119);
 const kSecondaryColor = Color.fromARGB(102, 218, 99, 23);
 const kSecondaryColorBackground = Color.fromARGB(26, 253, 170, 76);
 const kTextColor = Color.fromARGB(255, 9, 5, 28);
@@ -10,6 +11,7 @@ const kPlaceholderColor = Color.fromARGB(255, 218, 99, 23);
 const kShadowColor = Color.fromARGB(18, 90, 108, 234);
 const inActiveIconColor = Color.fromARGB(102, 83, 232, 139);
 const kBackgroundColor = Color.fromARGB(255, 250, 253, 255);
+const kBackgroundColor2 = Color.fromARGB(255, 254, 173, 29);
 const kDisableColor = Color.fromARGB(255, 217, 217, 217);
 const kDisableBackgroundColor = Color.fromARGB(255, 246, 246, 246);
 
--- lib/ui/order_details/components/body.dart
+++ lib/ui/order_details/components/body.dart
@@ -1,6 +1,9 @@
+import 'package:ffood/constants/constants.dart';
 import 'package:ffood/ui/components/header_with_back_button/header_with_back_button.dart';
 import 'package:ffood/ui/order_details/components/product_card_with_increment_counter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -12,13 +15,35 @@ class Body extends StatelessWidget {
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
-          children: const <Widget>[
-            HeaderWithBackButton(title: "Order Details"),
-            ProductCardWithIncrementCounter(
-              imageUrl: 'assets/images/PhotoMenu.png',
-              productName: 'Spacy fresh crab',
-              restaurantName: 'Warenik kita',
-              price: 220,
+          children: <Widget>[
+            const HeaderWithBackButton(title: "Order Details"),
+            Column(
+              children: const [
+                ProductCardWithIncrementCounter(
+                  imageUrl: 'assets/images/PhotoMenu.png',
+                  productName: 'Spacy fresh crab',
+                  restaurantName: 'Warenik kita',
+                  price: 220,
+                ),
+                ProductCardWithIncrementCounter(
+                  imageUrl: 'assets/images/PhotoMenu.png',
+                  productName: 'Spacy fresh crab',
+                  restaurantName: 'Warenik kita',
+                  price: 220,
+                ),
+                ProductCardWithIncrementCounter(
+                  imageUrl: 'assets/images/PhotoMenu.png',
+                  productName: 'Spacy fresh crab',
+                  restaurantName: 'Warenik kita',
+                  price: 220,
+                ),
+                ProductCardWithIncrementCounter(
+                  imageUrl: 'assets/images/PhotoMenu.png',
+                  productName: 'Spacy fresh crab',
+                  restaurantName: 'Warenik kita',
+                  price: 220,
+                ),
+              ],
             )
           ],
         ),
